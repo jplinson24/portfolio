ae2c829b1056eee6514b17a6b64617c85445fb6f
Your Name <youremail@example.com>
2024-07-10 10:25:20 +0800

    Updated Responsiveness

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,16 @@ document.getElementById('downloadBtn').addEventListener('click', function() {
   const resumeUrl = 'assets/pdf/LINSON_JOHN_PATRICK_RESUME.pdf';  
   window.open(resumeUrl, '_blank');
 });
+
+
+// Toggle the menu on click
+document.getElementById('menu').addEventListener('click', function () {
+  document.querySelector('nav').classList.toggle('active');
+});
+
+// Close the menu when a link is clicked
+document.querySelectorAll('nav ul a').forEach(item => {
+  item.addEventListener('click', function () {
+    document.querySelector('nav').classList.remove('active');
+  });
+});
